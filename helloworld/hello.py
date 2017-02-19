6ee3654c286cf832d16559214b421c8b9874490a
yathartha <yathartha32@gmail.com>
2017-02-19 11:41:00 +0530

    First commit

---
+++ helloworld/hello.py
@@ -0,0 +1,6 @@
+import os
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+class Hello(object):
+	def say_hello(self):
+		return "Hello World!!"
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+	name = 'helloworld',
+	version = '0.1',
+	packages = ['helloworld'],
+	description = 'helloworld testing',
+	) 
